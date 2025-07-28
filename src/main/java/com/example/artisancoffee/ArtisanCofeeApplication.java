320751df1850e7b9e6edf2d21ca09d7251323a4f
DariusBurnete <dariusburnete@yahoo.com>
2025-07-28 16:42:58 +0300

    chore(init): initial project setup with Docker Compose and base structure

---
+++ src/main/java/com/example/artisancoffee/ArtisanCofeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.artisancoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtisanCofeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtisanCofeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/artisancoffee/ArtisanCofeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artisancoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtisanCofeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
